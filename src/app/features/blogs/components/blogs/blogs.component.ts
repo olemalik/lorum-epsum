9d2d26825eec07025460a8cffcbca4700dcc0e67
olemalik <olemalik@gmail.com>
2022-06-15 02:35:21 +0530

    Blogs changes

--- src/app/features/blogs/components/blogs/blogs.component.ts
+++ src/app/features/blogs/components/blogs/blogs.component.ts
@@ -1,17 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { BlogsEndpoint } from '../../services/blogs.endpoint';
 import { Blogs } from '../../types/blogs';
 
 @Component({
   selector: 'app-blogs',
   templateUrl: './blogs.component.html',
-  styleUrls: ['./blogs.component.sass']
+  styleUrls: ['./blogs.component.sass'],
+  providers:[BlogsEndpoint]
 })
 export class BlogsComponent implements OnInit {
   @Input() blogsList: Blogs[] = []
 
-  constructor() { }
+  constructor(  private blogsEndpoint: BlogsEndpoint) { }
 
   ngOnInit(): void {
+    this.blogsEndpoint.getAllBlogs().subscribe((data: Blogs[]) => {
+      this.blogsList = data;
+      console.log(data)
+    })
   }
 
 }
